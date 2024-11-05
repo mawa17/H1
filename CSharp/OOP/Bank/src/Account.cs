c577846cfc5f567d9d322101695579c5ccdb0d87
mawa17 <163126157+mawa17@users.noreply.github.com>
2024-11-05 08:51:11 +0100

    push Bank Project

---
+++ CSharp/OOP/Bank/src/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public sealed class Account
+    {
+        public AccountOwner AccountOwner { get; }
+        public uint Balance { get; private set; }
+        public Account(in string surname, in string lastname, uint startBalance = 100)
+        {
+            this.Balance = Math.Max(startBalance, 100);
+            this.AccountOwner = new() { Surname = surname, Lastname = lastname };
+        }
+        public void Deposit(uint amount)
+        {
+            this.Balance += amount;
+        }
+        public bool Withdraw(uint amount)
+        {
+            if(this.Balance < amount) return false;
+            this.Balance -= amount;
+            return true;
+
+        }
+    }
+}
---
+++ CSharp/OOP/Bank/src/AccountOwner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public struct AccountOwner
+    {
+        public uint ClientId { get; }
+        public required string Surname { get; init; }
+        public required string Lastname { get; init; }
+        public AccountOwner()
+        {
+            this.ClientId = (uint)new Random().Next(0, 999999);
+        }
+    }
+}
