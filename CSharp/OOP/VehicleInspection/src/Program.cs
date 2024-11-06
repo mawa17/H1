6752bc5e2c9c14b6b85a5d61786712c4193ea55a
mawa17 <163126157+mawa17@users.noreply.github.com>
2024-11-06 09:33:31 +0100

    VehicleInspection Done

---
+++ CSharp/OOP/VehicleInspection/src/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace src
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string carBrand = "Audi";
+            string carModel = "A6";
+            DateOnly carProductionDate = new(2018, 7, 1);
+            DateTime carLastInspection = new(2022, 12, 8, 12, 34, 56);
+            Vehicle car = new Car(carBrand, carModel, carProductionDate, carLastInspection);
+
+            string truckBrand = "Volvo";
+            string truckModel = "A6";
+            DateOnly truckProductionDate = new(2018, 10, 1);
+            DateTime truckLastInspection = new(2021, 10, 1, 12, 34, 56);
+            Vehicle truck = new Truck(truckBrand, truckModel, truckProductionDate, truckLastInspection);
+
+            Console.WriteLine(car.DisplayInfo());
+            Console.WriteLine(car.InspectionStatus());
+            Console.WriteLine();
+            Console.WriteLine(truck.DisplayInfo());
+            Console.WriteLine(truck.InspectionStatus());
+        }
+    }
+}
---
+++ CSharp/OOP/VehicleInspection/src/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿namespace src
+{
+    public abstract class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateOnly ProductionDate { get; set; }
+        public DateTime LastInspection { get; set; }
+
+        private protected Vehicle(in string brand, in string model, in DateOnly productionDate, in DateTime lastInspection)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.ProductionDate = productionDate;
+            this.LastInspection = lastInspection;
+        }
+        public abstract string InspectionStatus();
+        public virtual string DisplayInfo() => $"Brand: {this.Brand}, Model: {this.Model}, Production Date: {this.ProductionDate.ToShortDateString()}";
+    }
+    public sealed class Car : Vehicle
+    {
+        public Car(in string brand, in string model, in DateOnly productionDate, in DateTime lastInspectionTime) : base(brand, model, productionDate, lastInspectionTime) { }
+        public override string InspectionStatus()
+        {
+            if (base.ProductionDate.Year > 4 && base.LastInspection.Year > 2) return $"{this.GetType().Name} NEED inspection ASAP and is NOT allowed to drive!";
+            return $"{this.GetType().Name} Is good to drive";
+        }
+        public override string DisplayInfo() => $"Car: {Brand} {Model}"; 
+
+    }
+    public sealed class Truck : Vehicle
+    {
+        public Truck(in string brand, in string model, in DateOnly productionDate, in DateTime lastInspectionTime) : base(brand, model, productionDate, lastInspectionTime) { }
+        public override string InspectionStatus()
+        {
+            if (base.ProductionDate.Year > 1 && base.LastInspection.Year > 1) return $"{this.GetType().Name} NEED inspection ASAP and is NOT allowed to drive!";
+            return $"{this.GetType().Name} Is good to drive";
+        }
+        public override string DisplayInfo() => $"Truck: {Brand} {Model}";
+    }
+}
