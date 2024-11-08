5f814b26b361d39e99dfb5ceb219b31de67e4916
mawa17 <163126157+mawa17@users.noreply.github.com>
2024-11-08 09:11:57 +0100

    polymorphism

---
+++ CSharp/OOP/Polymorphism/src/Animals.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    public static class SoundHelper
+    {
+        private static async Task PlaySoundAsync(string filePath)
+        {
+            await Task.Run(() =>
+            {
+                using (var soundPlayer = new SoundPlayer(filePath))
+                {
+                    soundPlayer.PlaySync();
+                }
+            });
+        }
+        public static async Task PlaySoundAsync(enmAnimals animal)
+        {
+            switch (animal)
+            {
+                case enmAnimals.Dog:
+                    await PlaySoundAsync("C:\\Users\\marcu\\Desktop\\H1\\CSharp\\OOP\\Polymorphism\\dog.wav");
+                    break;
+                case enmAnimals.Cat:
+                    await PlaySoundAsync("C:\\Users\\marcu\\Desktop\\H1\\CSharp\\OOP\\Polymorphism\\cat.wav");
+                    break;
+                case enmAnimals.Sheep:
+                    await PlaySoundAsync("C:\\Users\\marcu\\Desktop\\H1\\CSharp\\OOP\\Polymorphism\\sheep.wav");
+                    break;
+            }
+        }
+    }
+    public abstract class Animals
+    {
+        public abstract Task<string> MakeSoundAsync();
+        public string Name { get; init; }
+        protected Animals(in string name) => this.Name = name;
+    }
+
+    public class Dog(in string name) : Animals(name)
+    {
+        public override async Task<string> MakeSoundAsync()
+        {
+            await SoundHelper.PlaySoundAsync(enmAnimals.Dog);
+            return "Vow";
+        }
+    }
+    public class Cat(in string name) : Animals(name)
+    {
+        public override async Task<string> MakeSoundAsync()
+        {
+            await SoundHelper.PlaySoundAsync(enmAnimals.Cat);
+            return "Meau";
+        }
+    }
+    public class Sheep(in string name) : Animals(name)
+    {
+        public override async Task<string> MakeSoundAsync()
+        {
+            await SoundHelper.PlaySoundAsync(enmAnimals.Sheep);
+            return "Baaaah";
+        }
+    }
+    public enum enmAnimals
+    {
+        Dog = 1,
+        Cat = 2,
+        Sheep = 3,
+    }
+    public static class AnimalFactory
+    {
+        public static Animals? CreateAnimal(in string name, in enmAnimals animal) => animal switch
+        {
+            enmAnimals.Dog => new Dog(name),
+            enmAnimals.Cat => new Cat(name),
+            enmAnimals.Sheep => new Sheep(name),
+            _ => null,
+        };
+        public static Animals? CreateAnimal(in enmAnimals animal) => animal switch
+        {
+            enmAnimals.Dog => new Dog("Frida"),
+            enmAnimals.Cat => new Cat("Luna"),
+            enmAnimals.Sheep => new Sheep("Ella"),
+            _ => null,
+        };
+    }
+}
---
+++ CSharp/OOP/Polymorphism/src/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+
+namespace src
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Animals? dog = AnimalFactory.CreateAnimal("bob", enmAnimals.Dog);
+            Animals? cat = AnimalFactory.CreateAnimal("bo", enmAnimals.Cat);
+            Animals? sheep = AnimalFactory.CreateAnimal("bobby", enmAnimals.Sheep);
+            Console.WriteLine($"The dog {dog?.Name} says : {await dog?.MakeSoundAsync()}");
+            Console.WriteLine($"The cat {cat?.Name} says : {await cat?.MakeSoundAsync()}");
+            Console.WriteLine($"The sheep {sheep?.Name} says : {await sheep?.MakeSoundAsync()}");
+        }
+    }
+}
